23516c47f18029401deec1736e454aba14b73cb7
matmaz99 <62934060+matmaz99@users.noreply.github.com>
2025-04-29 11:52:08 +0200

    Create content-request.js

---
+++ src/api/content-request/routes/content-request.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * content-request router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::content-request.content-request'); 
